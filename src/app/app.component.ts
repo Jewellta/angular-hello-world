35cd7d4f8d437651ce6f40fc0a26033ab7edc35c
Tyler Jewell <jewelltyler15@gmail.com>
2021-04-01 19:22:44 -0400

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world-angular';
+  title = "hello-world-angular?";
+  name:string ="Tyler Jewell";
+
+myText:string="Default my Text string";
+
+  changeName():void{
+    this.name="tyler";
+  }
 }
